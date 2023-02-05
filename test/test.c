3bee13b1ab2391d3c605ce9991446a641ae034f6
Anatoly Rybchych <tol.ryb@gmail.com>
2023-02-05 14:07:39 +0200

    building as library

---
+++ test/test.c
@@ -0,0 +1,32 @@
+#include <glv.h>
+#include <glv/stack_panel.h>
+
+typedef struct{
+    int n;
+} AppContext;
+
+void app_manage(View *view, ViewMsg msg, void *ev, void *app_context){
+    AppContext *ctx = app_context;
+
+    switch (msg){
+    case VM_MOUSE_DOWN:
+        printf("click %i\n", ctx->n++);
+        break;
+    }
+}
+
+void app_init(View *root_view, void *app_context){
+    AppContext *ctx = app_context;
+    printf("ctx:%i\n", ctx->n);
+
+    glv_set_background(root_view, glv_gen_texture_solid_color(80, 160, 200, 255));
+}
+
+int main(void){
+    AppContext ctx = {
+        .n = 0,
+    };
+
+    glv_run(glv_stack_panel_proc, app_manage, &ctx, app_init);
+    return 0;
+}
